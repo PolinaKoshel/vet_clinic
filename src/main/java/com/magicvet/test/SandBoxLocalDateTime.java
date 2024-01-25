6c0fade9076e4fb35bb772f4977ec8f68b29efca
PolinaKoshel <pkoshel92@gmail.com>
2024-01-25 11:38:47 +0300

    REQ-09 Ability to add a few pets per client

--- src/main/java/com/magicvet/test/SandBoxLocalDateTime.java
+++
@@ -1,9 +0,0 @@
-package main.java.com.magicvet.test;
-
-import main.java.com.magicvet.model.Cat;
-
-public class SandBoxLocalDateTime {
-    public static void main(String[] args) {
-        System.out.println(new Cat());
-    }
-}
